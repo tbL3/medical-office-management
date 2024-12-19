98402950244fca6794e06c687d0311e676d9534d
t <t>
2024-12-19 14:01:22 +0100

    appointment repo

--- appointment-service/src/main/java/com/example/appointment/repository/AppointmentRepository.java
+++ appointment-service/src/main/java/com/example/appointment/repository/AppointmentRepository.java
@@ -1,4 +1,10 @@
 package com.example.appointment.repository;
 
-public class AppointmentRepository {
+import com.example.appointment.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 }
+
