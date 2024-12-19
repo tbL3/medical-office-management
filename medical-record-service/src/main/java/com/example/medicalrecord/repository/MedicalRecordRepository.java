af96c751c789fa04fdf8f54db754eed3c86d686d
t <t>
2024-12-19 14:15:55 +0100

    medical repo

--- medical-record-service/src/main/java/com/example/medicalrecord/repository/MedicalRecordRepository.java
+++ medical-record-service/src/main/java/com/example/medicalrecord/repository/MedicalRecordRepository.java
@@ -1,4 +1,10 @@
 package com.example.medicalrecord.repository;
 
-public class MedicalRecordRepository {
+import com.example.medicalrecord.model.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
 }
+
