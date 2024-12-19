4a5c0e8a5b0b08e496ec1a22a4309ab431228150
t <t>
2024-12-19 13:59:31 +0100

    appointment model

--- appointment-service/src/main/java/com/example/appointment/model/Appointment.java
+++ appointment-service/src/main/java/com/example/appointment/model/Appointment.java
@@ -1,4 +1,20 @@
 package com.example.appointment.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long patientId;
+    private Long practitionerId;
+    private Date date;
+
+    // Getters and Setters
 }
