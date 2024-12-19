01aebf22fd5e6afde3c5e368799dd041ffa8f084
t <t>
2024-12-19 13:23:45 +0100

    fix dockerfile

--- patient-service/src/main/java/com/example/patient/PatientApplication.java
+++ patient-service/src/main/java/com/example/patient/PatientApplication.java
@@ -1,4 +1,14 @@
 package com.example.patient;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class PatientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatientApplication.class, args);
+    }
 }
+
